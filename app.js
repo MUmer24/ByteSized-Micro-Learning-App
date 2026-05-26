75a81220bb85ec777e24da1479fa93b0213b9456
Muhammad Umer Khan <umer.m.khn24@gmail.com>
2026-05-26 17:05:34 +0500

    feat: build sequential UI layout and section transitions for user panel

--- app.js
+++ app.js
@@ -31,3 +31,46 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 });
+
+const viewWelcome = document.getElementById("welcome-section");
+const viewQuizRules = document.getElementById("quiz-rules-container");
+const viewQuiz = document.getElementById("quiz-container");
+const viewResults = document.getElementById("view-results");
+const viewTopicSelection = document.getElementById("topic-selection-container");
+
+const topicCards = document.querySelectorAll(".topic-card");
+const startQuizBtn = document.getElementById("start-quiz-btn");
+const quizCategoryBadge = document.getElementById("quiz-category-badge");
+
+let selectedCategory = "";
+
+topicCards.forEach((card) => {
+  card.addEventListener("click", () => {
+    // Remove 'selected' class from all cards
+    topicCards.forEach((c) => c.classList.remove("selected"));
+
+    // Add 'selected' class to clicked card
+    card.classList.add("selected");
+    console.log("Selected category: ", card);
+
+    // Save the selected category
+    selectedCategory = card.getAttribute("data-category");
+
+    // Update quiz category Button
+    startQuizBtn.removeAttribute("disabled");
+  });
+});
+
+startQuizBtn.addEventListener("click", ()=>{
+  if(!selectedCategory) return;
+
+  viewWelcome.classList.add("hidden");
+  viewQuizRules.classList.add("hidden");
+  viewTopicSelection.classList.add("hidden");
+  quizCategoryBadge.textContent = selectedCategory;
+
+  // Show Quiz 
+  viewQuiz.classList.remove("hidden");
+
+  console.log("Starting quiz for category: ", selectedCategory);
+});
